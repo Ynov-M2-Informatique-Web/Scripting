8ae84bffbabb6674040907d3ad154461cdc5a0b4
Co-7 <colin.v42@protonmail.com>
2022-10-14 12:51:19 +0200

    Add feature _alt exo 2

--- village.sh
+++ village.sh
@@ -4,29 +4,31 @@ if [ ! "$BASH_VERSION" ] ; then
    exec /bin/bash "$0" "$@"
 fi
 
+p=""
+if [ ! -z "${1}" ]
+   then
+      p="${1}"
+fi
+
 cd  "/home/ynov/"
 
 mkdir -p "colin"
-mkdir -p "colin/Ynov_campus"
+mkdir -p "colin/Ynov_campus$p"
 
-cd "/home/ynov/colin/Ynov_campus"
+cd "/home/ynov/colin/Ynov_campus$p"
 
-mkdir -p Mairie
-batiments_it=1
-batiments[0]="Mairie"
-mkdir -p Mairie/{Bureau,Archive}
+mkdir -p Mairie$p
+mkdir -p Mairie$p/{Bureau$p,Archive$p}
 
-mkdir -p Boulangerie
+mkdir -p Boulangerie$p
 
-batiments[1]="Boulangerie"
-mkdir -p Boulangerie/{Etalage,Laboratoire,Chambre-froide}
+mkdir -p Boulangerie$p/{Etalage$p,Laboratoire$p,Chambre-froide$p}
 
-habitants[0]='Maire;Mairie/Bureau/;Clef du bureau'
-habitants[1]='Assistant;Mairie/'
-habitants[2]='Boulanger;Boulangerie/Laboratoire/'
-habitants[3]='Vendeur;Boulangerie/Etalage/'
+habitants[0]="Maire$p;Mairie$p/Bureau$p/;Clef du bureau"
+habitants[1]="Assistant$p;Mairie$p/"
+habitants[2]="Boulanger$p;Boulangerie$p/Laboratoire$p/"
+habitants[3]="Vendeur$p;Boulangerie$p/Etalage$p/"
 
-batiments_it=2
 for i in "${habitants[@]}"
 do
    IFS=";" read -r -a arr <<< "${i}"
@@ -38,14 +40,12 @@ do
       echo "${arr[2]}"$'\n' > $path$name".personnage"
    fi
    mkdir -p "$name""_maison"
-   batiments[$batiments_it]=$name"_maison"
-   batiments_it=$((batiments_it+1))
 done
 
-objects[0]='levure;Boulangerie/Chambre-froide/'
-objects[1]='jambon;Boulangerie/Chambre-froide/'
-objects[2]='pain;Boulangerie/Laboratoire/'
-objects[3]='pain_jambon;Boulangerie/Laboratoire/'
+objects[0]="levure$p;Boulangerie$p/Chambre-froide$p/"
+objects[1]="jambon$p;Boulangerie$p/Chambre-froide$p/"
+objects[2]="pain$p;Boulangerie$p/Laboratoire$p/"
+objects[3]="pain_jambon$p;Boulangerie$p/Laboratoire$p/"
 
 for i in "${objects[@]}"
 do
@@ -55,14 +55,18 @@ do
    touch $path$name".objet"
 done
 
-ln -sf ../Chambre-froide/jambon.objet Boulangerie/Laboratoire
-ln -sf ../Chambre-froide/levure.objet Boulangerie/Laboratoire
-ln -sf ../Laboratoire/pain.objet Boulangerie/Etalage
-ln -sf ../Laboratoire/pain_jambon.objet Boulangerie/Etalage
+ln -sf ../Chambre-froide$p/jambon$p.objet Boulangerie$p/Laboratoire$p
+ln -sf ../Chambre-froide$p/levure$p.objet Boulangerie$p/Laboratoire$p
+ln -sf ../Laboratoire$p/pain$p.objet Boulangerie$p/Etalage$p
+ln -sf ../Laboratoire$p/pain_jambon$p.objet Boulangerie$p/Etalage$p
 
-touch "Mairie/Archive/registre.objet"
+touch "Mairie$p/Archive$p/registre$p.objet"
 
 echo "---
-habitants: ['Maire', 'Assistant', 'Boulanger', 'Vendeur']
-batiments: ['Maire_maison', 'Assistant_maison', 'Boulanger_maison', 'Vendeur_maison', 'Mairie', 'Boulangerie']" > "Mairie/Archive/registre.objet"
+habitants: ['Maire$p', 'Assistant$p', 'Boulanger$p', 'Vendeur$p']
+batiments: ['Maire_maison$p', 'Assistant_maison$p', 'Boulanger_maison$p', 'Vendeur_maison$p', 'Mairie$p', 'Boulangerie$p']" > "Mairie$p/Archive$p/registre$p.objet"
 
+if [ ! -z "${1}" ]
+   then
+      exec /bin/bash /mnt/d/Co/Documents/Dev/Ynov/Scripting/village_advance.sh _alt
+fi
--- village_advance.sh
+++ village_advance.sh
@@ -4,12 +4,18 @@ if [ ! "$BASH_VERSION" ]; then
     exec /bin/bash "$0" "$@"
 fi
 
-cd "/home/ynov/colin/Ynov_campus"
+p=""
+if [ ! -z "${1}" ]
+   then
+      p="${1}"
+fi
 
-getent passwd Archive || echo ynov | sudo -S useradd Archive
-echo ynov | sudo -S usermod -a -G Archive ynov
+cd "/home/ynov/colin/Ynov_campus$p"
 
-for i in Boulangerie Mairie; do
+getent passwd Archive$p || echo ynov | sudo -S useradd Archive$p
+echo ynov | sudo -S usermod -a -G Archive$p ynov
+
+for i in Boulangerie$p Mairie$p; do
     getent passwd $i || echo ynov | sudo -S useradd $i
     echo ynov | sudo -S usermod -a -G $i ynov
     echo ynov | sudo -S chown -R $i:$i $i
@@ -26,10 +32,17 @@ for i in $path; do
     echo ynov | sudo -S usermod -a -G $name ynov
 done
 
-getent passwd Ynov_campus || echo ynov | sudo -S useradd Ynov_campus
-echo ynov | sudo -S usermod -a -G Ynov_campus ynov
-echo ynov | sudo -S chown Ynov_campus:Ynov_campus "/home/ynov/colin/Ynov_campus"
-echo ynov | sudo -S chmod 750 -R "/home/ynov/colin/Ynov_campus"
+getent passwd Ynov_campus$p || echo ynov | sudo -S useradd Ynov_campus$p
+echo ynov | sudo -S usermod -a -G Ynov_campus$p ynov
+echo ynov | sudo -S chown Ynov_campus$p:Ynov_campus$p "/home/ynov/colin/Ynov_campus$p"
+echo ynov | sudo -S chmod 750 -R "/home/ynov/colin/Ynov_campus$p"
 echo ynov | sudo -S find . -type f -exec chmod 660 -- {} +
 
 echo ynov | su ynov &> /dev/null
+
+# exec /bin/bash /home/ynov/colin/village.sh _alt #! A mettre
+
+if [ -z "${1}" ]
+    then
+        exec /bin/bash /mnt/d/Co/Documents/Dev/Ynov/Scripting/village.sh _alt
+fi

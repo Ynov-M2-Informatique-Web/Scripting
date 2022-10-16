d886ad0f5f0ec02e2c945230a56d113b1e83a23e
Co-7 <colin.v42@protonmail.com>
2022-10-16 19:22:16 +0200

    Time correction and display of the number of users with an even uid

--- check_shell.sh
+++ check_shell.sh
@@ -6,5 +6,8 @@ fi
 
 TIMEFORMAT=%3lR
 
-r=`{ time date +%d:%m:%y ;} 2> time`
+r=`{ time date +%H:%M:%S ;} 2> time`
 echo "afficher l'heure au format \"00:00:00\" | $r | `cat time`" 
+
+`{ time cut -d: -f3 /etc/passwd | while read -r line; do if [ $(expr $line % 2) -eq 0 ]; then ((e=e+1)); echo $e > count ; fi; done } 2> time`
+echo "afficher le nombre d'user avec un uid pair | `cat count` | `cat time`"

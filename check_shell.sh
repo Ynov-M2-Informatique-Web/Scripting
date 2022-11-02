3896ffdf7c37891ee9689de4a9991f6711acaa0c
Co-7 <colin.v42@protonmail.com>
2022-11-02 10:40:18 +0100

    Improve script perf

--- check_shell.sh
+++ check_shell.sh
@@ -6,18 +6,12 @@ fi
 
 TIMEFORMAT=%3lR
 
-r=`{ time date +%H:%M:%S ;} 2> t`
-echo "afficher l'heure au format \"00:00:00\" | $r | `cat t`" 
-unset r
+time echo -n "afficher l'heure au format \"00:00:00\" | $(date +%H:%M:%S) | " 
 
-# `{ time cut -d: -f3 /etc/passwd | while read -r line; do if [ $(expr $line % 2) -eq 0 ]; then ((e=e+1)); echo $e > count ; fi; done } 2> time`
-# echo "afficher le nombre d'user avec un uid pair | `cat count` | `cat time`"
+time echo -n "afficher le nombre d'user avec un uid pair | $(gawk -F: '{  if ($3 % 2 == 0) print $3 }' /etc/passwd | wc -l) | "
 
-# `{ time cut -d: -f3 /etc/passwd | while read -r line; do echo $(expr $line % 2); done | $(grep -o 0 | wc -l > count) ;} 2> time`
-# echo "afficher le nombre d'user avec un uid pair | `cat count` | `cat time`"
+time echo -n "afficher le nombre de group avec gid impair | $(gawk -F: '{  if ($3 % 2 == 1) print $3 }' /etc/group | wc -l) | "
 
-`{ time cut -d: -f3 /etc/passwd | xargs -I {} expr {} % 2 | $(grep -c 0 > c) ;} 2> t`
-echo "afficher le nombre d'user avec un uid pair | `cat c` | `cat t`"
+time echo -n "afficher le nombre de fichier | $(find / -type -level0 f | wc -l) | "
 
-`{ time cut -d: -f3 /etc/group | xargs -I {} expr {} % 2 | $(grep -c 1 > c) ;} 2> t`
-echo "afficher le nombre de group avec gid impair | `cat c` | `cat t`"
+time echo -n "afficher le nombre de fichier | $(find / -type -level3 f | wc -l) | "

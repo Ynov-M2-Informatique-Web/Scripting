fb37b782ce427a35bd328890c79a178144cf0a0f
Co-7 <colin.v42@protonmail.com>
2022-11-03 09:33:43 +0100

    physical volume, lv root, packet loss

--- check_shell.sh
+++ check_shell.sh
@@ -32,7 +32,7 @@ time echo -n "afficher la taille du fichier en mode human_readeable | $(ls -lh r
 
 time echo -n "afficher le nombre de ligne de la commande man man | $(man -wa man | xargs -L1 zcat | wc -l) | "
 
-time echo -n "compter combien de fois le mot man est présent dans la commande man man | $(man -wa man | xargs -L1 zcat | grep man -cw) | "
+time echo -n "compter combien de fois le mot man est présent dans la commande man man | $(man -wa man | xargs -L1 zcat | grep man -ciow) | "
 
 time echo -n "cloner le repos grafana de github | $(wget -qO- https://github.com/grafana/grafana/archive/refs/heads/main.tar.gz | tar xz)"
 
@@ -43,6 +43,16 @@ time echo -n "récuperer ce fichier https://www.elastic.co/guide/en/elasticsearc
 time echo -n "récuperer ce fichier https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.4.3-darwin-x86_64.tar.gz | $(wget -q https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-8.4.3-darwin-x86_64.tar.gz)"
 
 time echo -n "Faire des 4 etapes precedentes un zip | $(zip -qq r.zip grafana-main release-notes-8.4.3.html elasticsearch-8.4.3-darwin-x86_64.tar.gz)"
+
+time echo -n "afficher le nom du physical volume | $(pvs -o pv_name --no-heading | sed 's/^ *//g') | "
+
+time echo -n "afficher l'uuid du volume group | $(vgdisplay | grep "VG UUID" | sed 's/[ \t]*VG UUID[ \t]*//g') | "
+
+time echo -n "afficher le path du lv rootp | $(lvdisplay | grep "LV Path" | sed 's/[ \t]*LV Path[ \t]*//g') | "
+
+time echo -n "afficher le nombre de paquet perdu en ping 8.8.8.8 | $(ping -c 1 -q 8.8.8.8 | grep -oP '[\d+.,]+(?=% packet loss)') | "
+
+time echo -n "afficher l'heure \"00:00:00\" | $(date +%H:%M:%S) | "
 
 echo -e '\033[0m'
 

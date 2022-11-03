bdf0c619b0c7ace4463899b0865dee8c944aa7ad
Co-7 <colin.v42@protonmail.com>
2022-11-03 16:50:20 +0100

    fix create service

--- create_trash_service.sh
+++ create_trash_service.sh
@@ -1,3 +1,3 @@
-mkdir /trash
-echo 'alias remove="/usr/bin/rm"'
-echo 'alias rm="mv \\!* /trash"'
+echo $'[Unit]\nDescription=MyTrash Daemon Colin\n[Service]\nType=simple\nUser=ynov\nExecStart=/home/ynov/colin/rm.sh\nRestart=on-failure\n\n[Install]\nWantedBy=default.target' > /etc/systemd/system/my_trash.service
+systemctl daemon-reload
+systemctl enable my_trash.service
---
+++ rm.sh
@@ -0,0 +1,3 @@
+mkdir /trash
+echo 'alias remove="/usr/bin/rm"'
+echo 'alias rm="mv \\!* /trash"'

a3ce0e59be8f1503e76638851ea994019971fbc7
Co-7 <colin.v42@protonmail.com>
2022-11-04 10:35:07 +0100

    clean trash crontab

--- rm.sh
+++ rm.sh
@@ -1,5 +1,6 @@
 mkdir -p /trash
 echo $'if [ -f $HOME/.bash_aliases ]\nthen\n  . $HOME/.bash_aliases\nfi' > /home/ynov/.bashrc
 echo 'alias remove="/usr/bin/rm"' > /home/ynov/.bash_aliases
-echo 'alias rm="mv \\!* /trash"' >> /home/ynov/.bash_aliases
+echo $'rm () {\n        sudo mv $1 /trash/\n}' >> /home/ynov/.bash_aliases
+(crontab -l 2>/dev/null; echo "0 * * * * /home/ynov/clean_trash.sh") | crontab -
 exec bash

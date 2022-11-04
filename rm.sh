98096edc21e3e512228a19aafc2af9b12f990980
Co-7 <colin.v42@protonmail.com>
2022-11-04 12:20:08 +0100

    update rm

--- rm.sh
+++ rm.sh
@@ -1,7 +1,8 @@
 mkdir -p /trash
 echo $'if [ -f $HOME/.bash_aliases ]\nthen\n  . $HOME/.bash_aliases\nfi' >> /home/ynov/.bashrc
 echo 'alias remove="/usr/bin/rm"' > /home/ynov/.bash_aliases
-echo $'rm () {\n        sudo mv $1 /trash/$1.$(date +%s)\n}' >> /home/ynov/.bash_aliases
+echo 'alias rm="rmm"' >> /home/ynov/.bash_aliases
+echo $'rmm () {\n        sudo mv $1 /trash/$1.$(date +%s)\n}' >> /home/ynov/.bash_aliases
 (crontab -l 2>/dev/null; echo "0 * * * * /home/ynov/clean_trash.sh") | crontab -
 exec bash
 

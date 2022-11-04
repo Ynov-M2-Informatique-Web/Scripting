b418adbcbe4db8d58c01c42cd40dd2a08656c316
Co-7 <colin.v42@protonmail.com>
2022-11-04 12:56:27 +0100

    remove exec

--- create_trash_service.sh
+++ create_trash_service.sh
@@ -10,4 +10,3 @@ echo 'alias rm="rmm"' >> /home/ynov/.bash_aliases
 echo $'rmm () {\nfichierSup200=$(($(stat -c %s $1) > 209715200))\n\n    if (($fichierSup200 == 1)); then\n        echo "remove $1"\n        remove $1\n    else\n        echo "move $1"\n        sudo mv $1 /trash/$1.$(date +%s)\n    fi\n}' >> /home/ynov/.bash_aliases
 # (crontab -l 2>/dev/null; echo "0 * * * * /home/ynov/clean_trash.sh") | crontab -
 source /home/ynov/.bashrc
-exec bash

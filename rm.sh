0d2070f6d5e59c7eaf873ca33efa778d9a6b49fa
Co-7 <colin.v42@protonmail.com>
2022-11-04 12:07:09 +0100

    delete instant > 200mo

--- rm.sh
+++ rm.sh
@@ -4,3 +4,15 @@ echo 'alias remove="/usr/bin/rm"' > /home/ynov/.bash_aliases
 echo $'rm () {\n        sudo mv $1 /trash/$1.$(date +%s)\n}' >> /home/ynov/.bash_aliases
 (crontab -l 2>/dev/null; echo "0 * * * * /home/ynov/clean_trash.sh") | crontab -
 exec bash
+
+rm () {
+    fichierSup200=$(($(stat -c %s $1) > 209715200))
+
+    if (($fichierSup200 == 1)); then
+        echo "remove $1"
+        remove $1
+    else
+        echo "move $1"
+        sudo mv $1 /trash/$1.$(date +%s)
+    fi    
+}

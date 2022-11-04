3be6ea68b4cb598130091de4d1a10abd4ff25eca
Co-7 <colin.v42@protonmail.com>
2022-11-04 10:12:28 +0100

    rename fail

--- rm.sh
+++ rm.sh
@@ -1,5 +1,5 @@
 mkdir /trash
-echo $'if [ -f $HOME/.bash_aliases ]\nthen\n  . $HOME/.bash_aliases\nfi' > /home/ynov/.bash_rc
+echo $'if [ -f $HOME/.bash_aliases ]\nthen\n  . $HOME/.bash_aliases\nfi' > /home/ynov/.bashrc
 echo 'alias remove="/usr/bin/rm"' >> /home/ynov/.bash_aliases
 echo 'alias rm="mv \\!* /trash"' >> /home/ynov/.bash_aliases
 exec bash
